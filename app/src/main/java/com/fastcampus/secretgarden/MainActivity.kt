4381e87d39a9df5084f9fef05c38fcf15f97156d
hongjae kang <james.h_kang@icloud.com>
2021-04-25 18:15:09 +0900

    update diary activity

--- app/src/main/java/com/fastcampus/secretgarden/MainActivity.kt
+++ app/src/main/java/com/fastcampus/secretgarden/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fastcampus.secretgarden
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,6 +10,7 @@ import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,30 +64,49 @@ class MainActivity : AppCompatActivity() {
 
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
 
-            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
 
-            if (passwordPreferences.getString("password","000").equals(passwordFromUser)) {
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
 
-                //startActivity()
+                startActivity((Intent(this, DiaryAcitivity::class.java)))
 
             } else {
-                AlertDialog.Builder(this)
-                    .setTitle("실패")
-                    .setMessage("비밀번호가 잘 못 되었습니다.")
-                    .setPositiveButton("확인") { _, _ -> }
-                    .create()
-                    .show()
+                showErrorAlertDialog()
             }
         }
 
         changePasswordButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
             if (changePasswordMode) {
+                passwordPreferences.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
 
             } else {
-
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                } else {
+                    showErrorAlertDialog()
+                }
             }
         }
+    }
 
+    private fun showErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호가 잘 못 되었습니다.")
+            .setPositiveButton("확인") { _, _ -> }
+            .create()
+            .show()
     }
 
 }
